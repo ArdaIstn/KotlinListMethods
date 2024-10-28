a00d609411245a8b636f741ee35e6a4c5d580eec
Arda <arda_isitan55@hotmail.com>
2024-10-28 13:22:26 +0300

    Added forEachIndexed method output.

--- app/src/main/java/com/example/kotlinlistmethods/listmethods/forEachIndexed.kt
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/forEachIndexed.kt
@@ -11,4 +11,14 @@ fun main() {
      * Bazı durumlarda indexlerine de ihtiyacımız olabilir.
      * Index ve ona karşılık gelen sayıları almamız sağlar.
      */
+
+    /**
+     * Çıktısı:
+     * Index: 0, Value: 1
+     * Index: 1, Value: 2
+     * Index: 2, Value: 3
+     * Index: 3, Value: 4
+     * Index: 4, Value: 5
+     *
+     */
 }
