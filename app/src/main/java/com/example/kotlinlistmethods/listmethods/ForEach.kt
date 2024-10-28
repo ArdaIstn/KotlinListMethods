afa4834d551f8819fa69f8cbbae17625adfa21cb
Arda <arda_isitan55@hotmail.com>
2024-10-28 11:46:43 +0300

    Added ForEach method.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/ForEach.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinlistmethods.listmethods
+
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+
+    list.forEach {
+        println(it)
+    }
+
+    /**
+     * Liste içerisindeki elemanları tek tek almamız sağlar.
+     * it ifadesi listedeki her bir elemanı temsil eder.
+     */
+
+}
