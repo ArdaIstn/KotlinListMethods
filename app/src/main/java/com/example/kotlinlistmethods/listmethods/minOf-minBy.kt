200af3a5e0110b861b7e8889b5fe8bcc8c27859b
Arda <arda_isitan55@hotmail.com>
2024-10-28 13:18:02 +0300

    Added minOf-minBy method.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/minOf-minBy.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinlistmethods.listmethods
+
+data class User5(val name: String, val age: Int)
+
+fun main() {
+    val users = listOf(
+        User5("Alice", 30), User5("Bob", 25), User5("Charlie", 35)
+    )
+
+    val result = users.minOf {
+        it.age
+    }
+    println(result)
+
+    val result2 = users.minBy {
+        it.age
+    }
+    println(result2)
+
+    /**
+     * Aynı şekilde minOf bize liste içerisindeki en küçük yaşı döndürür.
+     * minBy ise en küçük yaşa sahip data class nesnesini bize döndürür.
+     */
+
+}
