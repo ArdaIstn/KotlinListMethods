4622366d3cc27b4e05b1a68e790367333c63980e
Arda <arda_isitan55@hotmail.com>
2024-10-28 12:02:57 +0300

    Added findLast method.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/findLast.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinlistmethods.listmethods
+
+fun main() {
+    val list = listOf("a", "ab", "c")
+    val result = list.findLast { it.contains("a") }
+    println(result)
+
+    /**
+     * Bu metodda ise,liste içerisindeki tüm elemanları gezer ve son a içeren elemanı bize geri döner.
+     *
+     */
+}
