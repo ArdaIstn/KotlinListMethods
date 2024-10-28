bc9d3d9a5f8538a527d828bd011d4a612a2f28f2
Arda <arda_isitan55@hotmail.com>
2024-10-28 12:39:32 +0300

    Added reversed methods.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/reversed.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinlistmethods.listmethods
+
+fun main() {
+    val list = listOf("a", "b", "c")
+
+    val result = list.reversed()
+    println(list)
+    println(result)
+
+    /**
+     * Listenin tersini alıp bize bir liste şeklinde geri döner.
+     */
+}
