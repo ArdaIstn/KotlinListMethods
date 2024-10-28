9aaea60bcdff531e32d78c9fa9bc76f1caa273ea
Arda <arda_isitan55@hotmail.com>
2024-10-28 12:36:55 +0300

    Added map methods.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/map.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinlistmethods.listmethods
+
+data class User(val name: String, val age: Int)
+data class UserUI(val name: String)
+
+fun main() {
+
+    val users = listOf(
+        User("Arda", 25), User("Mehmet", 30), User("Ahmet", 35)
+    )
+    val mappedUsers = users.map {
+        UserUI(it.name)
+    }
+    println(mappedUsers)
+
+    val ageList = users.map {
+        it.age
+    }
+    println(ageList)
+
+    val nameList = users.map {
+        it.name
+    }
+    println(nameList)
+
+    /**
+     * User classında eğer age'a ihtiyacımız yoksa,bu classı UI için oluşturduğumuz data classa dönüştürmemizi sağlar.
+     * Bu sayede yeni bir liste oluştururuz fakat bu listede sadece name bilgileri bulunur.
+     * Yani burda,mapin içerisinde belirttiğimiz türde bir liste oluşur.
+     */
+
+
+}
