73ae4ef8d8cd53e083105510e6a978554b3c1f5a
Arda <arda_isitan55@hotmail.com>
2024-10-28 12:41:14 +0300

    Added shuffled methods.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/shuffled.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinlistmethods.listmethods
+
+fun main() {
+    val list = listOf("a", "b", "c")
+
+    val result = list.shuffled()
+    println(list)
+    println(result)
+
+    /**
+     * Listenin sıralamasını değiştirip,bize bir liste şeklinde geri döner.
+     */
+}
