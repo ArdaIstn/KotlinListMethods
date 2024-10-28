b81ff14a71780ad0a1d3135f235b478bcec13d46
Arda <arda_isitan55@hotmail.com>
2024-10-28 12:57:05 +0300

    Edit sumOf method.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/sumOf.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinlistmethods.listmethods
+
+data class User3(val name: String, val age: Int)
+
+fun main() {
+
+    val users = listOf(
+        User3("Arda", 25), User3("Ahmet", 30), User3("Mehmet", 20)
+    )
+
+    val result = users.sumOf {
+        it.age
+    }
+    println(result)
+
+    /**
+     * sumOf metoduyla birlikte de,verilen listenin içerisindeki nesnelerin age bilgilerini toplayıp bir değişkene atayabiliriz.
+     */
+
+}
