541d97701e3ac15522e01f09e6fa521bb452d5cd
Arda <arda_isitan55@hotmail.com>
2024-10-28 13:13:02 +0300

    Added maxOf-maxBy metod.

---
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/maxOf-maxBy.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinlistmethods.listmethods
+
+data class User4(val name: String, val age: Int)
+
+fun main() {
+
+    val users = listOf(
+        User4("Alice", 25), User4("Bob", 30), User4("Charlie", 35)
+
+    )
+    val result = users.maxOf {
+        it.age
+    }
+    val result2 = users.maxBy {
+        it.age
+    }
+    println(result)
+    println(result2)
+
+
+    /**
+     * maxOf metodu bu örnekten yola çıkarsak,users listesindeki en büyük yaşı bize geri döndürür.
+     * maxBy metodu ise users listesindeki en büyük yaşa sahip data class nesnesini bize geri dönüdürür,item'ın kendisini döndürür.
+     */
+
+}
