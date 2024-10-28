c8e717307ea6f693be186e9c508a4b9ff997a474
Arda <arda_isitan55@hotmail.com>
2024-10-28 13:24:56 +0300

    Added groupBy method output.

--- app/src/main/java/com/example/kotlinlistmethods/listmethods/groupBy.kt
+++ app/src/main/java/com/example/kotlinlistmethods/listmethods/groupBy.kt
@@ -3,14 +3,19 @@ package com.example.kotlinlistmethods.listmethods
 fun main() {
     val list = listOf("a", "a", "c")
 
-    val result = list.groupBy { it == "a" }.values
+    val result = list.groupBy { it == "a" }
     println(result)
 
     /**
      * İçerisinde a harfi olanları gruplamamızı sağlar ve çıktı olarak bir map döner.
+     * Bu örnek için result,Map<Boolean, List<String>> şeklinde döner.
      * İçerisinde a olanları true,olmayanları ise false şeklinde bize geri döner.
-     * .keys dersek sadece true ve false,.values dersek ise onlara karşılık gelen değerleri almış oluruz.
-     *
+     * .keys dersek sadece true ve false, .values dersek ise onlara karşılık gelen değerleri almış oluruz.
+     */
+
+    /**
+     * Çıktısı
+     * {true=[a, a], false=[c]}
      */
 
 }
